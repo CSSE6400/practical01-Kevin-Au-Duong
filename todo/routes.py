96e2dd968239431256d1a72d9af6394b24e304dc
Kevin-Au-Duong <69077371+Kevin-Au-Duong@users.noreply.github.com>
2023-02-22 01:57:22 +0000

    Finished jsonify section

--- todo/routes.py
+++ todo/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 @api.route('/health')
 def health():
-    return "ok"
+    return jsonify({'status': 'ok'})
