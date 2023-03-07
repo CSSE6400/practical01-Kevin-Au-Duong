9c3796798d45e8b1ff3e9f108833497fd8991a90
Kevin-Au-Duong <69077371+Kevin-Au-Duong@users.noreply.github.com>
2023-03-07 15:07:25 +0000

    Fixed project structure

--- todo/__init__.py
+++ todo/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     
-    from .routes import api
+    from .views.routes import api
     app.register_blueprint(api)     
 
     return app
